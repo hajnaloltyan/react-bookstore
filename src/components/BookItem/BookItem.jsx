e17a62c581b92e7be4c1c983cf3caffacf7e00ed
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-07-15 01:04:32 +0200

    Create: map over the books, and create them dinamically, and add deleteBook function

--- src/components/BookItem/BookItem.jsx
+++ src/components/BookItem/BookItem.jsx
@@ -1,23 +1,36 @@
 import './BookItem.css';
 
-const BookItem = () => {
+const BookItem = ({ bookProp, deleteBook }) => {
+  const {
+    id,
+    title,
+    author,
+    category,
+    currentChapter,
+    completed,
+  } = bookProp;
+
   return (
     <li className="bookCard">
       <article className="bookDetails">
         <div>
-          <h4 className="bookInfo">{book.category}</h4>
-          <h3 className="bookInfo">{book.title}</h3>
-          <p className="bookInfo">{book.author}</p>
+          <h4 className="bookInfo">{category}</h4>
+          <h3 className="bookInfo">{title}</h3>
+          <p className="bookInfo">{author}</p>
 
           <ul className="actionButtons">
             <li><button type="button">Comments</button></li>
-            <li><button type="button">Remove</button></li>
+            <li>
+              <button type="button" onClick={() => deleteBook(id)}>
+                Remove
+              </button>
+            </li>
             <li><button type="button">Edit</button></li>
           </ul>
         </div>
 
         <div className="bookCompletion">
-          <h2>{book.completed}</h2>
+          <h2>{completed}</h2>
           <h3>Completed</h3>
         </div>
       </article>
@@ -26,7 +39,7 @@ const BookItem = () => {
 
       <div className="bookChapter">
         <h4>Current Chapter</h4>
-        <h4>{book.currentChapter}</h4>
+        <h4>{currentChapter}</h4>
         <button type="button" className="progressBtn">Update Progress</button>
       </div>
     </li>
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -3,14 +3,38 @@ import BookItem from '../BookItem/BookItem';
 import './Books.css';
 
 const Books = () => {
-  const [books, setBooks] = useState({
-    id: 0,
-    title: 'title',
-    author: 'author',
-    category: 'Action',
-    currentChapter: 'Chapter 3',
-    completed: '10%',
-  });
+  const [books, setBooks] = useState([
+    {
+      id: 0,
+      title: 'title',
+      author: 'author',
+      category: 'Action',
+      currentChapter: 'Chapter 3',
+      completed: '10%',
+    },
+    {
+      id: 1,
+      title: 'title',
+      author: 'author',
+      category: 'Action',
+      currentChapter: 'Chapter 3',
+      completed: '10%',
+    },
+    {
+      id: 2,
+      title: 'title',
+      author: 'author',
+      category: 'Action',
+      currentChapter: 'Chapter 3',
+      completed: '10%',
+    },
+  ]);
+
+  const deleteBook = (id) => {
+    setBooks([
+      ...books.filter((book) => book.id !== id),
+    ]);
+  };
 
   return (
     <ul className="bookList">
@@ -18,6 +42,7 @@ const Books = () => {
         <BookItem
           key={book.id}
           bookProp={book}
+          deleteBook={deleteBook}
         />
       ))}
     </ul>
