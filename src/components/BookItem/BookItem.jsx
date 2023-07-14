bab0c7fe968f789aafab138ba3f9a74e73c5254d
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-07-15 00:13:50 +0200

    Style: normalise margins and paddings, and add basic styles for bookItem

--- src/components/BookItem/BookItem.jsx
+++ src/components/BookItem/BookItem.jsx
@@ -1,3 +1,5 @@
+import './BookItem.css';
+
 const BookItem = () => {
   const book = {
     id: 0,
@@ -9,32 +11,33 @@ const BookItem = () => {
   };
 
   return (
-    <li>
-      <article>
+    <li className="bookCard">
+      <article className="bookDetails">
         <div>
-          <h4>{book.category}</h4>
-          <h3>{book.title}</h3>
-          <p>{book.author}</p>
-          <ul>
+          <h4 className="bookInfo">{book.category}</h4>
+          <h3 className="bookInfo">{book.title}</h3>
+          <p className="bookInfo">{book.author}</p>
+
+          <ul className="actionButtons">
             <li><button type="button">Comments</button></li>
             <li><button type="button">Remove</button></li>
             <li><button type="button">Edit</button></li>
           </ul>
         </div>
 
-        <div>
+        <div className="bookCompletion">
           <h2>{book.completed}</h2>
           <h3>Completed</h3>
         </div>
-
-        <hr />
-
-        <div>
-          <h4>Current Chapter</h4>
-          <h4>{book.currentChapter}</h4>
-          <button type="button">Update Progress</button>
-        </div>
       </article>
+
+      <hr className="card-hr" />
+
+      <div className="bookChapter">
+        <h4>Current Chapter</h4>
+        <h4>{book.currentChapter}</h4>
+        <button type="button" className="progressBtn">Update Progress</button>
+      </div>
     </li>
   );
 };
