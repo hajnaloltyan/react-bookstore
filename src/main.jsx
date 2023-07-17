44b326ba39cd42cffa6e20b6eb3ffc3918e23602
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-07-17 23:42:49 +0200

    Install: Redux and update store with created reducers

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from './redux/store';
+import store from './redux/store';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import './index.css';
